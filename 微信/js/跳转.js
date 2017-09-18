f60dc80f9a857d2e4d35e782f0a9051f08f912b3
alex_zhao <1475978622@qq.com>
2017-09-18 14:12:10 +0800

    增加了JavaScript内容，实现了聊天，图灵机器人对话，发送图片，地图定位，发表朋友圈新动态及评论点赞功能

---
+++ 微信/js/跳转.js
@@ -0,0 +1,19 @@
+    
+	
+	var index=document.getElementById("index");
+	var addresslist=document.getElementById("addresslist");
+	var discovery=document.getElementById("discovery");
+	var me=document.getElementById("me");
+	
+	index.onclick=function(){
+		window.location.href="复制 index.html";
+	}
+	addresslist.onclick=function(){
+		window.location.href="addresslist.html";
+	}
+	discovery.onclick=function(){
+		window.location.href="discovery.html";
+	}
+	me.onclick=function(){
+		window.location.href="me.html";
+	}
